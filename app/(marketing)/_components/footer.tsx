465af108879a4af96adfeca9d1630d165dab33b9
Easierq <isiaqofficial@gmail.com>
2025-02-27 18:44:56 +0100

    new updates

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -6,12 +6,20 @@ export const Footer = () => {
     <div className="w-full p-4 py-3 border-t bg-slate-100">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+        <div className="space-x-2 md:w-auto hidden md:flex items-center">
           <Button size="sm" variant="ghost">
             Privacy Policy
           </Button>
           <Button size="sm" variant="ghost">
             Terms of Service
+          </Button>
+        </div>
+        <div className="space-x-1 md:w-auto flex md:hidden items-center">
+          <Button size="sm" variant="ghost">
+            Privacy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms
           </Button>
         </div>
       </div>
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
   return (
-    <div className="z-10 fixed top-0 w-full h-14 px-4 pr-0 border-b shadow-sm bg-white flex items-center">
+    <div className="z-10 fixed top-0 w-full h-14 px-4 pr-0 shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         {/* <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full"> */}
